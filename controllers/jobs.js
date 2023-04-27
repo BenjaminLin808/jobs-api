e92164ee6c986e8aaaf0c1a9e67d1bd898d8a89f
BenjaminLin808 <benjaminlin808@gmail.com>
2023-04-27 00:47:32 -0700

    initial commit

---
+++ controllers/jobs.js
@@ -0,0 +1,26 @@
+const Job = require("../models/Job");
+const { StatusCodes } = require("http-status-codes");
+const { BadRequestError, NotFoundError } = require("../errors");
+
+const getAllJobs = async (req, res) => {
+  res.send("get all jobs");
+};
+
+const getJob = async (req, res) => {
+  res.send("get job");
+};
+
+const createJob = async (req, res) => {
+  const job = await Job.create({ ...req.body, createdBy: req.user.userId });
+  res.status(StatusCodes.CREATED).json({ job });
+};
+
+const updateJob = async (req, res) => {
+  res.send("update job");
+};
+
+const deleteJob = async (req, res) => {
+  res.send("delete job");
+};
+
+module.exports = { getAllJobs, getJob, createJob, updateJob, deleteJob };
