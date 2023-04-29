d56544175ecd97b9bbcb001929fca43a9bcd58a3
BenjaminLin808 <benjaminlin808@gmail.com>
2023-04-29 00:48:32 -0700

    continue on the crud

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -3,7 +3,8 @@ const { StatusCodes } = require("http-status-codes");
 const { BadRequestError, NotFoundError } = require("../errors");
 
 const getAllJobs = async (req, res) => {
-  res.send("get all jobs");
+  const jobs = await Job.find({ createdBy: req.user.userId }).sort("createdAt");
+  res.status(StatusCodes.OK).json({ jobs, count: jobs.length });
 };
 
 const getJob = async (req, res) => {
