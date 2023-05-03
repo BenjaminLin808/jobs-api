10e9807cd246bd00b8659f760b52ca7b6b04ab74
BenjaminLin808 <benjaminlin808@gmail.com>
2023-05-02 23:12:05 -0700

    ex

--- app.js
+++ app.js
@@ -30,6 +30,9 @@ app.use(cors());
 app.use(xss());
 
 // routes
+app.get("/", (req, res) => {
+  res.send("Welcome to the Job Board API");
+});
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/jobs", authenticateUser, jobsRouter);
 
